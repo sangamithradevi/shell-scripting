595195f0869806080d41402c101cc420bb4940a3
sangamithradevi <sanga.nayini@gmail.com>
2020-05-02 09:37:05 +0530

    commit3

--- stack.sh
+++ stack.sh
@@ -3,9 +3,9 @@
 LOG=/tmp/stack.log
 rm -f $LOG
 
-## check if script is executed by the root user ot not
+## check if script is executed by the root user or not
 USER_ID=$(id -u)
-if[$USER_ID -eq 0]; then
+if [ $USER_ID -eq 0 ]; then
 echo "you loged in as root user"
 else
 echo "login as root user or use sudo command to execute commands"
@@ -13,7 +13,7 @@ exit 1
 fi
 
 yum install httpd -y &>>$LOG
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
 echo "command executed"
 else
 echo "command failed"
