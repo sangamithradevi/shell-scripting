34fd3816479ac9eddfbebce6e363f7c0f5a7738a
sangamithradevi <sangamithradevi1987@gmail.com>
2020-05-02 08:24:50 +0530

    changes made

--- stack.sh
+++ stack.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 ## check if script is executed by the root user ot not
+<<COMMENT
 if(uid==0)
 then 
 echo "logged in as user"
 else
-echo "login as user to execute the script"
+echo "login as user to execute the script" 
+COMMENT
